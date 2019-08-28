857ef206e46b71f721143880c971864e69dce7a9
tdnicola <tdnicola@gmail.com>
2019-08-28 11:17:31 -0400

    updated js file with showTyping function for animation.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 //scrolling
-    window.onscroll = function() { stickyBar() };
+window.onscroll = function() { stickyBar() };
     // Get the navbar
-    window.addEventListener('scroll', stickyBar);
+window.addEventListener('scroll', stickyBar);
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyBar() {
     var navbar = document.getElementById("menubar");
     var sticky = navbar.offsetTop;
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function stickyBar() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
-        } else {
-            navbar.classList.remove("sticky");
-        }
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
     }
+};
+
+//waiting until page is loaded for typewritter effect
+function showTyping() {
+    var typeWriter = document.querySelector('.typewriter');
+    typeWriter.classList.remove('hidden');
+}
+
+console.log('hello')
+window.addEventListener('load', showTyping);
