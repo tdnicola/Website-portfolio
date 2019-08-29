099dae6f29d5201f040546472f77366eb14f8df3
tdnicola <tdnicola@gmail.com>
2019-08-29 08:34:03 -0400

    changed navbar to load at top. css changes.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,10 @@
-//scrolling
-window.onscroll = function() { stickyBar() };
-    // Get the navbar
-window.addEventListener('scroll', stickyBar);
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-var navbar = document.getElementById("menubar");
-var sticky = navbar.offsetTop;
-
-function stickyBar() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-};
 
 //waiting until page is loaded for typewritter effect
 function showTyping() {
     var typeWriter = document.querySelector('.typewriter');
-    typeWriter.classList.remove('hidden');
+    setTimeout(function() {
+        typeWriter.classList.remove('hidden');
+    }, 500 );
 }
 
-window.onload = showTyping;
+window.addEventListener('load', showTyping);
