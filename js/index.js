5d935051a1d6f575fd15c3932d56fbb4334b123e
tdnicola <tdnicola@gmail.com>
2019-09-15 15:37:42 -0400

    working on updating about me section.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-setTimeout(function(){
-  new TypeIt('.typing')
-    .type('&lt;/web&gt; Developer')
-    .pause(1000)
-    .delete()
-    .type('Developer')
-    .pause(1000)
-    .delete()
-    .type('Likes to code')
-    .go();
-}, 500);
+document.addEventListener("DOMContentLoaded", function(event) { 
+    setTimeout(function(){
+    new TypeIt('.typing')
+      .type('&lt;/web&gt; Developer')
+      .pause(1000)
+      .delete()
+      .type('Developer')
+      .pause(1000)
+      .delete()
+      .type('Likes to code')
+      .go();
+  }, 500);
+});
